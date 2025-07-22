78851d2f520346b34a396cfd9ad7a1a31c7d3183
OgGG-rj <ggwithrj@gmail.com>
2025-07-22 18:21:38 +0530

    server.js

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const path = require('path');
 
+// Serve static files from "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-const users = {};
-
-io.on('connection', (socket) => {
-    const userId = socket.id;
-    const username = "User_" + Math.floor(Math.random() * 10000);
-    users[userId] = username;
-
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', { user: users[userId], text: msg });
-    });
-
-    socket.on('disconnect', () => {
-        delete users[userId];
-    });
+// Fallback route to index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-http.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
